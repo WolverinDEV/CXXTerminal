edcf2935203b0e64180fb1b5199b490d5db053ce
WolverinDEV <git@mcgalaxy.de>
2019-07-01 17:27:55 +0200

    improved libevent build file

--- libraries/build_event.sh
+++ libraries/build_event.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+original_dir=$(pwd)
+cd $(dirname $0)
+
 cd event
-if ! [ -d build ]
-then
+if ! [[ -d build ]]; then
 	mkdir build
 fi
 cd build
 cmake .. -DCMAKE_BUILD_TYPE="Debug" -DBUILD_TESTING=OFF -DEVENT__DISABLE_BENCHMARK=ON -DEVENT__DISABLE_SAMPLES=ON -DCMAKE_C_FLAGS="-fPIC" -DEVENT__DISABLE_TESTS=ON -DEVENT__DISABLE_OPENSSL=ON
+[[ $? -ne 0 ]] && exit 1
 make clean
+[[ $? -ne 0 ]] && exit 1
 make -j 12
+[[ $? -ne 0 ]] && exit 1
+cd ${original_dir}
